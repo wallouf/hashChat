41443feb75724399fe5479642360d5c7d0996261
wallouf <ct13860@gmail.com>
2014-07-15 20:51:40 +0200

    Init repository

---
+++ src/hachagewallouf/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hachagewallouf;
+
+/**
+ *
+ * @author a807087
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HachageFrame hF = new HachageFrame();
+    }
+
+}
